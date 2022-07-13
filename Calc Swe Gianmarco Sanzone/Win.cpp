4def8f2095835b0d3222aa1e4b464081f2eb8155
GianmarcoSanzone <sanzonegianmarco@gmail.com>
2022-07-13 19:44:52 -0400

    Bfactory merged into main branch

--- Calc Swe Gianmarco Sanzone/Win.cpp
+++ Calc Swe Gianmarco Sanzone/Win.cpp
@@ -1,5 +1,6 @@
 #include "Win.h"
 #include "wx/wx.h"
+#include "Bfac.h"
 
 wxBEGIN_EVENT_TABLE(Win, wxFrame)
 EVT_BUTTON(10000, Win::OnButtonClicked)
@@ -12,6 +13,7 @@ EVT_BUTTON(10006, Win::OnButtonClicked)
 EVT_BUTTON(10007, Win::OnButtonClicked)
 EVT_BUTTON(10008, Win::OnButtonClicked)
 EVT_BUTTON(10009, Win::OnButtonClicked)
+EVT_BUTTON(10010, Win::OnButtonClicked)
 EVT_BUTTON(10011, Win::OnButtonClicked)
 EVT_BUTTON(10012, Win::OnButtonClicked)
 EVT_BUTTON(10013, Win::OnButtonClicked)
@@ -22,55 +24,21 @@ EVT_BUTTON(10017, Win::OnButtonClicked)
 EVT_BUTTON(10018, Win::OnButtonClicked)
 EVT_BUTTON(10019, Win::OnButtonClicked)
 EVT_BUTTON(10020, Win::OnButtonClicked)
-EVT_BUTTON(10021, Win::OnButtonClicked)
 
 
 wxEND_EVENT_TABLE()
 
-Win::Win() : wxFrame(nullptr, wxID_ANY, "TI-84 Calculator", wxPoint(200, 200), wxSize(555, 700))
+Win::Win() : wxFrame(nullptr, wxID_ANY, "TI-84 Calculator", wxPoint(200, 200), wxSize(555, 700), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE))
 {
-	//numbers
-	button0 = new wxButton(this, 10000, "0", wxPoint(110, 110), wxSize(100, 100));
-	button1 = new wxButton(this, 10001, "1", wxPoint(220, 110), wxSize(100, 100));
-	button2 = new wxButton(this, 10002, "2", wxPoint(330, 110), wxSize(100, 100));
-	button3 = new wxButton(this, 10003, "3", wxPoint(110, 220), wxSize(100, 100));
-	button4 = new wxButton(this, 10004, "4", wxPoint(220, 220), wxSize(100, 100));
-	button5 = new wxButton(this, 10005, "5", wxPoint(330, 220), wxSize(100, 100));
-	button6 = new wxButton(this, 10006, "6", wxPoint(110, 330), wxSize(100, 100));
-	button7 = new wxButton(this, 10007, "7", wxPoint(220, 330), wxSize(100, 100));
-	button8 = new wxButton(this, 10008, "8", wxPoint(330, 330), wxSize(100, 100));
-	button9 = new wxButton(this, 10009, "9", wxPoint(220, 440), wxSize(100, 100));
 
-
-
+	Bfac f;
 	//text box
+	txtbox = new wxTextCtrl(this, 10000, "", wxPoint(0,5), wxSize(590,100));
+	txtbox->Disable();
 
-	txtbox = new wxTextCtrl(this, 10010, "", wxPoint(0,5), wxSize(590,100));
+	Win::SetMaxSize(wxSize(555, 700));
 
-
-
-	//operators
-
-	//left hand
-	add = new wxButton(this, 10011, "+", wxPoint(0, 110), wxSize(100, 100));
-	subtract = new wxButton(this, 10012, "-", wxPoint(0, 220), wxSize(100, 100));
-	multiply = new wxButton(this, 10013, "*", wxPoint(0, 330), wxSize(100, 100));
-	divide = new wxButton(this, 10014, "/", wxPoint(110, 440), wxSize(100, 100));
-
-	//Right hand
-	mod = new wxButton(this, 10015, "%", wxPoint(440, 110), wxSize(100, 100));
-	equals = new wxButton(this, 10017, "=", wxPoint(440, 220), wxSize(100, 100));
-	negative = new wxButton(this, 10018, "-x", wxPoint(330, 440), wxSize(100, 100));
-
-	//bottom
-	clear = new wxButton(this, 10016, "Clear", wxPoint(220, 550), wxSize(100, 100));
-	
-
-	//BIN HEX AND DEC
-
-	dec = new wxButton(this, 10019, "Dec", wxPoint(0, 440), wxSize(100, 100));
-	Hex = new wxButton(this, 10020, "HEX", wxPoint(440, 330), wxSize(100, 100));
-	Bin = new wxButton(this, 10021, "BIN", wxPoint(440, 440), wxSize(100, 100));
+	f.BM(this);
 
 
 }
@@ -133,55 +101,55 @@ void Win::OnButtonClicked(wxCommandEvent &event)
 			txtbox->AppendText("9");
 			break;
 
-		case 10011:
+		case 10010:
 
 			txtbox->AppendText("+");
 			break;
 
-		case 10012:
+		case 10011:
 
 			txtbox->AppendText("-");
 			break;
 
-		case 10013:
+		case 10012:
 
 			txtbox->AppendText("*");
 			break;
 
-		case 10014:
+		case 10013:
 
 			txtbox->AppendText("/");
 			break;
 
-		case 10015:
+		case 10014:
 
 			txtbox->AppendText("%");
 			break;
 
-		case 10016:
+		case 10015:
 
 			txtbox->Clear();
 			break;
 
-		case 10017:
+		case 10016:
 
 			txtbox->AppendText("EQUALS: ");
 			break;
 
-		case 10018:
+		case 10017:
 
 			txtbox->AppendText("-");
 			break;
 
-		case 10019:
+		case 10018:
 			txtbox->AppendText(".");
 			break;
 
-		case 10020:
+		case 10019:
 			txtbox->AppendText("HEX");
 			break;
 
-		case 10021:
+		case 10020:
 			txtbox->AppendText("BIN");
 			break;
 	}
